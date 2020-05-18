71cac03e42a1b8c6781e489ee8a10e522a751dda
ripper-s <ripper-s@ripper-s.com>
2020-05-18 14:55:02 +0800

    1

---
+++ reverseLeftWords/src/a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+	char* reverseLeftWords(char* s, int n){
+		int len = strlen(s),i = 0;;
+		char *ans = malloc(sizeof(char) * (len + 1));
+		while(i < len){
+			*(ans++) = s[(n + i++)%len];
+		}
+		*ans= '\0';
+		return ans - len;
+	}
+}
