97c10cc962bf13ef5c9dae6db341f53cbacfc090
Ripper_S <ripper-s@ripper-s.com>
2020-07-05 02:54:43 +0800

    update Date/linklist code

--- Date/linklist/src/node.c
+++ Date/linklist/src/node.c
@@ -1,61 +1,124 @@
-#include "node.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node {
+	int val;
+	struct node *next;
+}Node;
 
-void add(List *pList, int number)
+typedef struct list {
+	Node *head;
+//	Node *tail;
+}List;
+
+void create_node(List *pList, int data)
 {
-	// add to linked-list
-	Node *p=(Node*)malloc(sizeof(Node));
-	p->value=number;
-	p->next=NULL;
-	// find the last
-	Node *last=pList->head;
-	if(last){
+//	add to linked-list
+	Node *p = (Node*)malloc(sizeof(Node));
+	p->val = data;
+	p->next = NULL;
+//	find the last
+	Node *last = pList->head;
+	if (last) {
 		while(last->next){
-			last=last->next;
+			last = last->next;
 		}
-		// attach
-		last->next=p;
+		last->next = p;		// add tail node
 	}else{
-		pList->head=p;
+		pList->head = p;	// head node
 	}
+}
+
+void del_node(List *pList, int number)
+{
+	Node *pre = pList->head;
+	Node *temp;
+	for (int i = 1; i < number-1; i++) {
+		pre = pre->next;
+	}
+
+	if (pre->next) {
+		temp = pre->next;
+		pre->next = pre->next->next;
+		free(temp);
+	} else {
+		printf("node not exist!!!");
+	}
+}
+
+void ins_node(List *pList, int number, int data)
+{
+	Node *pre = pList->head;
+	Node *p = (Node*)malloc(sizeof(Node));
+	p->val = data;
+	p->next = NULL;
+
+	for (int i = 1; i < number-1; i++) {
+		pre = pre->next;
+	}
+
+	p->next = pre->next;
+	pre->next = p;
+}
+
+void change_node(List *pList, int number, int data)
+{
+	Node *pre = pList->head;
+	for (int i = 1; i < number-1; i++) {
+		pre =pre->next;
+	}
+	
+	if (pre->next) {
+		pre->next->val = data;
+	} else {
+		printf("node not exist!!!");
+	}
+}
+
+void free_node(List *pList)
+{
+	Node *pre = pList->head;
+	Node *temp;
+	while (pre) {
+		temp = pre;
+		pre = pre->next;
+		free(temp);
+	}
+	pList->head = NULL;
 }
 
 void printf_node(List *pList)
 {
 	Node *p;
 	for(p=pList->head; p; p=p->next){
-		printf("%d ", p->value);
+		printf("%d ", p->val);
 	}
 	printf("\n");
 }
 
+
 int main(int argc, char const *argv[])
 {
-	int number;
-	List list;
-	list.head=NULL;
-	do {
-		scanf("%d", &number);
-		if (number != -1) {
-			add(&list, number);
-		}
-	} while (number != -1);
-	printf_node(&list);
+	List L;
+	L.head=NULL;
+	int n;
 
-/*
-	Node *p;
-	for (p=list.head; p; p=p->next) {
-		if (p->value = number) {
-			printf("OhYeah! Find it!\n");
-			break;
-		} else {
-			printf("404 NOT FOUND");
-			break;
+	do {
+		scanf("%d", &n);
+		if (n != 0) {
+			create_node(&L, n);
 		}
-	}
-*/
+	} while (n != 0);
+	
+	ins_node(&L, 2, 2);
+	del_node(&L, 3);
+	change_node(&L, 3, 3);
+
+	printf_node(&L);
+
+	free_node(&L);
+	printf("after free\n");
+	printf_node(&L);
 
 	return 0;
 }
